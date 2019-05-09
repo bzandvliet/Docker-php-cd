6b1c16d5aeeaf6d44614ce6f80be666e23f89ad6
Bas Zandvliet <b.zandvliet@previder.nl>
2019-05-09 14:46:37 +0200

    Change

--- php-cd/includes/config.php
+++ php-cd/includes/config.php
@@ -8,7 +8,7 @@
 function config($key = '')
 {
     $config = [
-        'name' => 'Simple PHP on Docker Website',
+        'name' => 'Simple PHP App on Docker Website',
         'site_url' => '',
         'pretty_uri' => true,
         'nav_menu' => [
